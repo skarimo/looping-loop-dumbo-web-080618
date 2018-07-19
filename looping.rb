30548f30de61e6576ed7caa51fa3d55d6d2002d8
Sherzod Karimov <karimovj@gmail.com>
2018-07-19 15:02:53 +0000

    automatically backed up by learn

--- looping.rb
+++ looping.rb
@@ -1,6 +1,6 @@
 def looping
 	loop do
-	  puts "( It's your first year at Hogwarts and you're having a tough time getting the hang of that levitation spell. Fill out the content of the method `looping` to contain a `loop` that `puts` the levitation charm `"Wingardium Leviosa"` an infinite number of times. Beneath your method definition, call your method by typing the name of the method, `looping`. )"
+	  puts "a tough time getting the hang of that levitation spell. Fill out the content of the method `looping` to contain a `loop` that `puts` the levitation charm `"Wingardium Leviosa"` an infinite number of times. Beneath your method definition, call your method by typing the name of the method, `looping`. )"
 end
 end
 
